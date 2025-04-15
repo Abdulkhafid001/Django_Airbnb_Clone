b19a2836c83f47bd378965a6f64ec25a1c727e55
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-04-15 15:39:41 +0100

    add auth folder

---
+++ airbnb_clone/airbnb_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AirbnbAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'airbnb_auth'
--- airbnb_clone/listings/views.py
+++ airbnb_clone/listings/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def home(request):
-    return HttpResponse('Welcome Home Yslcodes')
+    if request.user.is_authenticated:
+        return HttpResponse('Welcome Home Yslcodes')
+    else:
+        return redirect('login', preserve_request=True)
