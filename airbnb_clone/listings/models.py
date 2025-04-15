80180c1a1a75cb24265a6a3151d1dd9d18382157
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-04-15 17:44:07 +0100

    add data to models

--- airbnb_clone/listings/models.py
+++ airbnb_clone/listings/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 
+class Property(models.Model):
+    # host = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
