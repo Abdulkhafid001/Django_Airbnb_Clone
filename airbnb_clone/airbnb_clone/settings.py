af9c5d774ebb740538515e54a08f5ccc7707fab1
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-04-21 22:06:57 +0100

    setting up chat functionality

--- airbnb_clone/airbnb_clone/settings.py
+++ airbnb_clone/airbnb_clone/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat',
     'listings',
     'airbnb_auth',
     'django.contrib.admin',
--- airbnb_clone/airbnb_clone/urls.py
+++ airbnb_clone/airbnb_clone/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('listings.urls')),
+    path('airbnbclone/chat', include('chat.urls')),
     path('auth/', include('airbnb_auth.urls')),
-    path("accounts/", include("allauth.urls")), # new
+    path("accounts/", include("allauth.urls")),  # new
     path('admin/', admin.site.urls),
 ]
