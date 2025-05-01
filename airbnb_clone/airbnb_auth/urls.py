2c0b5633f1836e11167cc0b35c50db863d16bc6c
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-05-01 11:44:14 +0100

    configure new ws chat functionality

--- airbnb_clone/airbnb_auth/urls.py
+++ airbnb_clone/airbnb_auth/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, logout_page, signup_view, login_page
 urlpatterns = [
-    path("", Home.as_view(), name="home")
+    path("", Home.as_view(), name="home"),
+    path('login/', login_page, name="login"),
+    path('logout/', logout_page, name="logout"),
+    path('signup/', signup_view, name="signup"),
 ]
--- airbnb_clone/chat/models.py
+++ airbnb_clone/chat/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Message(models.Model):
     sender = models.ForeignKey(
-        User, related_name="sent_messages", on_delete=models.CASCADE)
+        User, related_name="sent_messages", on_delete=models.CASCADE, default=1)
     receiver = models.ForeignKey(
-        User, related_name="received_messages", on_delete=models.CASCADE)
+        User, related_name="received_messages", on_delete=models.CASCADE, default=1)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
