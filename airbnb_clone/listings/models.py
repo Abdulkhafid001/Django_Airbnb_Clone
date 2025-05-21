f00d2bdb7c9345a01e32d09e4a8ac1f289d4bf36
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-05-21 12:16:57 +0100

    add signals for notifications

--- airbnb_clone/listings/models.py
+++ airbnb_clone/listings/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from listings.signals import listing_added
 
 
 class Property(models.Model):
     # host = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
+    slug = models.SlugField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            listing_added.send(sender=self)
+            super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
-
--- airbnb_clone/notifications/signals.py
+++ airbnb_clone/notifications/signals.py
@@ -1,6 +1,9 @@
 from django.dispatch import receiver
 from listings.signals import listing_added
+
+
 @receiver(listing_added)
 def send_listing_added_notification(sender, **kwargs):
     '''signal to send a neotification when a listing is added.'''
-    
+    listing = sender.property
+    print('listing: ', listing)
