1468eb039afc5a7583d18a7d9a29d7aae412f5c9
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-05-21 11:35:10 +0100

    add notifications config

---
+++ airbnb_clone/listings/signals.py
@@ -0,0 +1,3 @@
+from django.dispatch import Signal
+
+listing_added = Signal()
---
+++ airbnb_clone/notifications/signals.py
@@ -0,0 +1,6 @@
+from django.dispatch import receiver
+from listings.signals import listing_added
+@receiver(listing_added)
+def send_listing_added_notification(sender, **kwargs):
+    '''signal to send a neotification when a listing is added.'''
+    
